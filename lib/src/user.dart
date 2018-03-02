41756ff8ac7047f1f170582b54556380e498b7cc
Ethan Lafrenais <elafrenais12@gmail.com>
2018-03-02 17:32:05 -0500

    Remove setters for User properties since it doesn't affect anything

--- lib/src/user.dart
+++ lib/src/user.dart
@@ -2,16 +2,12 @@ import 'dart:js' as js;
 
 class User {
   String get displayableId => _handle['displayableId'];
-  set displayableId(String value) => _handle['displayableId'] = value;
 
   String get name => _handle['name'];
-  set name(String value) => _handle['name'] = value;
 
   String get identityProvider => _handle['identityProvider'];
-  set identityProvider(String value) => _handle['identityProvider'] = value;
 
   String get userIdentifier => _handle['userIdentifier'];
-  set userIdentifier(String value) => _handle['userIdentifier'] = value;
 
   /// Gets the Dart wrapper of the underlying User JavaScript object.
   js.JsObject get jsHandle => _handle;
