a149d08ee8141ffbccfb906055fa8bd14aab31b9
Ethan Lafrenais <elafrenais12@gmail.com>
2018-03-02 16:34:08 -0500

    Fix User.fromJsObject constructor
    The User constructor is not exposed via msal.js, so we can't validate the object.

--- lib/src/user.dart
+++ lib/src/user.dart
@@ -1,7 +1,5 @@
 import 'dart:js' as js;
 
-import 'msal_context.dart';
-
 class User {
   String get displayableId => _handle['displayableId'];
   set displayableId(String value) => _handle['displayableId'] = value;
@@ -22,13 +20,10 @@ class User {
 
   User._(this._handle);
 
+  /// Creates a wrapper over a JavaScript MSAL user object.
+  /// 
+  /// Note: Intended for internal use only.
   factory User.fromJsObject(js.JsObject object) {
-    final js.JsObject jsConstructor = msalHandle['User'];
-
-    if (object['constructor'] != jsConstructor) {
-      throw new ArgumentError.value(object, 'object', 'JsObject must be of type Msal.User.');
-    }
-
     return new User._(object);
   }
 }
